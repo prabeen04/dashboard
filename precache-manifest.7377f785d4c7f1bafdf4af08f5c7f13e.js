6a9f0d7c7afa9391198b888eda9d22a90d7eba00
Prabeen04 <prabeen.strange@gmail.com>
2019-05-22 10:30:44 +0530

    Updates

---
+++ precache-manifest.7377f785d4c7f1bafdf4af08f5c7f13e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c986b4dcb6fbdac69f8b0d15f183f3",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "d63a5a00a4a333b2ed5d",
+    "url": "/dashboard/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "1db78b9414e4d66a1823",
+    "url": "/dashboard/static/css/main.b95b8395.chunk.css"
+  },
+  {
+    "revision": "d63a5a00a4a333b2ed5d",
+    "url": "/dashboard/static/js/2.e560eb31.chunk.js"
+  },
+  {
+    "revision": "1db78b9414e4d66a1823",
+    "url": "/dashboard/static/js/main.2be1f65b.chunk.js"
+  },
+  {
+    "revision": "dcfbc05562333eb34d98",
+    "url": "/dashboard/static/js/runtime~main.26f3d777.js"
+  }
+]);
