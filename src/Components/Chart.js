4731df259ff2c03e58302e88d472641708f1e63a
Prabeen04 <prabeen.strange@gmail.com>
2019-05-22 10:32:38 +0530

    gh-pages

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Empty } from "antd";
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from 'recharts';
 
 export default function Chart({ data, dataKey, value, emptyText, width, height, ...rest }) {
   if (!data.length) return <Empty description={emptyText || 'No data'} />
   return (
-    <div>
+    <ResponsiveContainer width='100%' aspect={5.0/3.0}>
       <LineChart
-        width={width || '100%'}
-        height={height || '100%'}
+        // width={width || 500}
+        // height={height || 300}
         data={data}
         margin={{
           top: 5, right: 30, left: 20, bottom: 5,
@@ -24,6 +24,6 @@ export default function Chart({ data, dataKey, value, emptyText, width, height, 
         <Legend />
         <Line type="monotone" dataKey={value} stroke="#8884d8" activeDot={{ r: 8 }} />
       </LineChart>
-    </div>
+    </ResponsiveContainer>
   )
 }
--- src/Components/DateRangePicker.js
+++ src/Components/DateRangePicker.js
@@ -4,12 +4,16 @@ import moment from 'moment';
 const RangePicker = DatePicker.RangePicker;
 const dateFormat = 'YYYY/MM/DD';
 
-export default function DateRangePicker({ startDate, endDate, onChange }) {
+export default function DateRangePicker({ onChange }) {
+  function disabledDate(current) {
+    // Can not select future date
+    return current && current > moment().endOf('day');
+  }
   return (
     <div>
       <RangePicker
-        // defaultValue={[moment(startDate, dateFormat), moment(endDate, dateFormat)]}
         onChange={onChange}
+        disabledDate={disabledDate}
       />
     </div>
   )
